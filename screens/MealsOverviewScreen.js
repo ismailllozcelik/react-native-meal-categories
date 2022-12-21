ef2bc44b33e7ec7fcd46b6d0202da301ad77e3d2
ismailllozcelik <ismaillll.ozcelik@gmail.com>
2022-12-21 17:30:30 +0300

    navigation added basically

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+function MealsOverviewScreen({ route }) {
+    const catId = route.params.categoryId;
+  return (
+    <View style={styles.container}>
+      <Text>Meals Overview Screen - {catId} </Text>
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
